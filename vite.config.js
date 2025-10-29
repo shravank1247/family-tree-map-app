d0ce8b2283021760956b7bc5cc846a9023b4cc02
ShravanKumarT <49195502+shravank1247@users.noreply.github.com>
2025-10-29 20:21:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
-  //base: process.env.NODE_ENV === 'production' ? '/family-tree-map-app/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/family-tree-map-app/' : '/',
   base: '/family-tree-map-app/',
   plugins: [react()],
   // server: {
@@ -11,5 +11,3 @@ export default defineConfig({
   //   open: true
   // }
 })
-
-
