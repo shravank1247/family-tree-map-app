0376d534931303696f764a86cb45280a79319d6a
shravank1247 <shravan2indar@gmail.com>
2025-10-29 19:58:15 +0530

    "Updated build params"

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/family-tree-map-app/' : '/',
-  // base: '/family-tree-map-app/',
+  //base: process.env.NODE_ENV === 'production' ? '/family-tree-map-app/' : '/',
+  base: '/family-tree-map-app/',
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  }
+  // server: {
+  //   port: 5173,
+  //   open: true
+  // }
 })
 
 
